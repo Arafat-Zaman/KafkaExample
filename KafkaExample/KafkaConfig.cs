e19974bb341d01d342867eb69958c14a8c572331
Arafat Zaman Anik <arafat.zaman.anik@gmail.com>
2024-10-03 04:42:54 +0600

    Add project files.

---
+++ KafkaExample/KafkaConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaExample
+{
+    public class KafkaConfig
+    {
+        public const string BootstrapServers = "localhost:9092"; // Kafka server address
+        public const string Topic = "test_topic";  // Kafka topic to produce/consume from
+    }
+
+}
---
+++ KafkaExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Collections.Generic;
+using Confluent.Kafka;
+using KafkaExample;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var producerTask = ProduceMessagesAsync();
+
+        // Wait for producer to finish
+        await producerTask;
+
+        // Now, start consuming messages
+        ConsumeMessages();
+    }
+
+    static void ConsumeMessages()
+    {
+        var config = new ConsumerConfig
+        {
+            GroupId = "test-consumer-group",
+            BootstrapServers = KafkaConfig.BootstrapServers,
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            consumer.Subscribe(KafkaConfig.Topic);
+            Console.WriteLine("Consuming messages from Kafka...");
+
+            while (true)
+            {
+                var consumeResult = consumer.Consume(CancellationToken.None);
+                Console.WriteLine($"Consumed message '{consumeResult.Message.Value}' from: {consumeResult.TopicPartitionOffset}");
+            }
+        }
+    }
+
+    static async Task ProduceMessagesAsync()
+    {
+        // Producer code remains the same as in Step 4
+    }
+}
